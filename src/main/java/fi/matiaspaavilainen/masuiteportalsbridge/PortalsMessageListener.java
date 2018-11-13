a4ed7a909caa402a74cb4280a55fa5acd3241303
Masa <masa@simasasolutions.com>
2018-11-13 20:35:06 +0200

    Fixed a lot of issues

--- src/main/java/fi/matiaspaavilainen/masuiteportalsbridge/PortalsMessageListener.java
+++ src/main/java/fi/matiaspaavilainen/masuiteportalsbridge/PortalsMessageListener.java
@@ -8,6 +8,8 @@ import org.bukkit.plugin.messaging.PluginMessageListener;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PortalsMessageListener implements PluginMessageListener {
 
@@ -30,27 +32,36 @@ public class PortalsMessageListener implements PluginMessageListener {
                 String childchannel = in.readUTF();
                 if (childchannel.equals("CreatePortal")) {
                     String[] p = in.readUTF().toLowerCase().split(":");
-                    Portal portal = new Portal();
-                    portal.setName(p[0]);
-                    portal.setType(p[1]);
-                    portal.setDestination(p[2]);
-                    portal.setFillType(p[3]);
-                    portal.setMinLoc(new Location(Bukkit.getWorld(p[4]), Double.parseDouble(p[5]), Double.parseDouble(p[6]), Double.parseDouble(p[7])));
-                    portal.setMaxLoc(new Location(Bukkit.getWorld(p[4]), Double.parseDouble(p[8]), Double.parseDouble(p[9]), Double.parseDouble(p[10])));
                     if (Bukkit.getWorld(p[4]) != null) {
-                        if (PortalManager.portalNames.contains(portal.getName())) {
-                            return;
+                        if (PortalManager.portalNames.contains(p[0])) {
+                            if (PortalManager.portals.values().stream().map(portals1 -> portals1.stream().filter(portal -> portal.getName().equalsIgnoreCase(p[0])).findFirst().get()).findFirst().isPresent()) {
+                                Portal por = PortalManager.portals.values().stream().map(portals1 -> portals1.stream().filter(portal -> portal.getName().equalsIgnoreCase(p[0])).findFirst().get()).findFirst().get();
+                                PortalManager.removePortal(por);
+                                por.setType(p[1]);
+                                por.setDestination(p[2]);
+                                por.setFillType(p[3]);
+                                por.setMinLoc(new Location(Bukkit.getWorld(p[4]), Double.parseDouble(p[5]), Double.parseDouble(p[6]), Double.parseDouble(p[7])));
+                                por.setMaxLoc(new Location(Bukkit.getWorld(p[4]), Double.parseDouble(p[8]), Double.parseDouble(p[9]), Double.parseDouble(p[10])));
+                                PortalManager.portals.get(Bukkit.getWorld(p[4])).add(por);
+                            }
+                        } else {
+                            Portal portal = new Portal(p[0], p[1], p[2], p[3], new Location(Bukkit.getWorld(p[4]), Double.parseDouble(p[5]), Double.parseDouble(p[6]), Double.parseDouble(p[7])), new Location(Bukkit.getWorld(p[4]), Double.parseDouble(p[8]), Double.parseDouble(p[9]), Double.parseDouble(p[10])));
+                            PortalManager.portals.get(Bukkit.getWorld(p[4])).add(portal);
                         }
-                        PortalManager.portals.get(Bukkit.getWorld(p[4])).add(portal);
+                        PortalManager.portalNames.add(p[0]);
                         PortalManager.loadPortals();
                     } else {
                         System.out.println("World not found");
                     }
                 }
                 if (childchannel.equals("DeletePortal")) {
+                    System.out.println("Received");
                     String p = in.readUTF();
+                    System.out.println(p);
                     if (PortalManager.portalNames.contains(p)) {
-                        PortalManager.portals.forEach((world, portals) -> portals.stream().filter(portal -> portal.getName().equals(p)).filter(PortalManager::removePortal));
+                        System.out.println(p);
+                        Portal por = PortalManager.portals.values().stream().map(portals1 -> portals1.stream().filter(portal -> portal.getName().equalsIgnoreCase(p)).findFirst().get()).findFirst().get();
+                        PortalManager.removePortal(por);
                     }
                 }
             }
